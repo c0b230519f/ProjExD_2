7beaf79f8afb6b01b4c11d5fbad9b15fafb09600
kousuke <c0b230519f@edu.teu.ac.jp>
2024-11-26 14:33:53 +0900

    爆弾

--- dodge_bomb.py
+++ dodge_bomb.py
@@ -1,13 +1,14 @@
 import os
+import random
 import sys
 import pygame as pg
 
 
 WIDTH, HEIGHT = 1100, 650
 DELTA = {
-    pg.K_UP: (0, -5),
-    pg.K_DOWN: (0, +5),
-    pg.K_LEFT: (-5, 0),
+    pg.K_UP:    (0, -5),
+    pg.K_DOWN:  (0, +5),
+    pg.K_LEFT:  (-5, 0),
     pg.K_RIGHT: (+5, 0)
 }
 os.chdir(os.path.dirname(os.path.abspath(__file__)))
@@ -20,6 +21,14 @@ def main():
     kk_img = pg.transform.rotozoom(pg.image.load("fig/3.png"), 0, 0.9)
     kk_rct = kk_img.get_rect()
     kk_rct.center = 300, 200
+    bb_img = pg.Surface((20,20))
+    pg.draw.circle(bb_img,(255,0,0),(10,10),10)
+    bb_img.set_colorkey((0, 0, 0))
+    bb_rct = bb_img.get_rect()
+    bb_rct.center = random.randint(0, WIDTH), random.randint(0, HEIGHT)
+
+    vx, vy = 5, 5
+
     clock = pg.time.Clock()
     tmr = 0
     while True:
@@ -34,8 +43,17 @@ def main():
             if key_lst[key]:
                 sum_mv[0] += delta[0]
                 sum_mv[1] += delta[1]
+            
         kk_rct.move_ip(sum_mv)
+
+        bb_rct.move_ip(vx, vy)
+        if bb_rct.left < 0 or bb_rct.right > WIDTH:
+            vx = -vx
+        if bb_rct.top < 0 or bb_rct.bottom > HEIGHT:
+            vy = -vy
+
         screen.blit(kk_img, kk_rct)
+        screen.blit(bb_img, bb_rct)
         pg.display.update()
         tmr += 1
         clock.tick(50)
